47e7700bcfd883e718b2be39144d44b26d49ac60
Szymon Pulka <szymon@hued.me>
2022-11-15 11:36:59 +0100

    dog

---
+++ ConsoleApp3/ConsoleApp2/Dog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Dog
+    {
+        public int Age { get; set; }
+        public string Name { get; private set; } = "Azor";
+
+        public void SetName(string name)
+        {
+            GetVoice();
+            Name = name;
+        }
+
+        private void GetVoice()
+        {
+            Console.WriteLine("Hau");
+        }
+    }
+}
--- ConsoleApp3/ConsoleApp2/Program.cs
+++ ConsoleApp3/ConsoleApp2/Program.cs
@@ -7,6 +7,7 @@ Console.WriteLine("Hello, World!");
 
 int x = 0;
 
+//////////// PERSON ///////////
 Person szymon = new Person(75, 190);
 szymon.age = 27;
 szymon.name = "Szymon";
@@ -14,3 +15,15 @@ szymon.name = "Szymon";
 double szymonBMI = szymon.GetBMI();
 Console.WriteLine(szymonBMI);
 szymon.ShowAll();
+//////////// PERSON ///////////
+
+
+//////// DOG ////////////
+Dog azor = new Dog();
+azor.Age = 27;
+
+Console.WriteLine(azor.Name);
+azor.SetName("Kubuś");
+Console.WriteLine(azor.Name);
+
+//////// DOG ////////////
