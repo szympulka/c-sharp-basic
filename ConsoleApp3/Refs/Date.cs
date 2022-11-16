f37a4328993a840cdbecd4fc297f72f7c6efab32
Szymon Pulka <szymon@hued.me>
2022-11-16 10:29:30 +0100

    date

---
+++ ConsoleApp3/Refs/Date.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Refs
+{
+    internal static class Date
+    {
+        public static void ShowDate()
+        {
+            var now = DateTime.Now.AddDays(22).AddHours(44).AddMinutes(123456);
+            var dayOfWeek = now.DayOfWeek;
+            var dayofweek = now.ToString("dddd");
+
+            string favoriteTask = now.DayOfWeek switch
+            {
+                DayOfWeek.Sunday => "Niedziela",
+                DayOfWeek.Monday => throw new NotImplementedException(),
+                DayOfWeek.Tuesday => throw new NotImplementedException(),
+                DayOfWeek.Wednesday => throw new NotImplementedException(),
+                DayOfWeek.Thursday => throw new NotImplementedException(),
+                DayOfWeek.Friday => throw new NotImplementedException(),
+                DayOfWeek.Saturday => throw new NotImplementedException(),
+            };
+
+            Console.WriteLine(favoriteTask);
+        }
+    }
+}
--- ConsoleApp3/Refs/Program.cs
+++ ConsoleApp3/Refs/Program.cs
@@ -1,41 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//// See https://aka.ms/new-console-template for more information
+//using Refs;
+
+//Console.WriteLine("Hello, World!");
+
+//int x = 5;
+//Console.WriteLine(x);
+//Console.WriteLine("Wysyłamy kopię do funkcji");
+//var newValue = Incremnt(x);
+//Console.WriteLine(newValue);
+
+//int Incremnt(int x)
+//{
+//    return x += 5;
+//}
+
+
+//string text = "5fd";
+
+//bool isGood = int.TryParse(text, out int parseValue);
+//if (isGood == true)
+//{
+//    Console.WriteLine("Działa!");
+//}
+//else
+//{
+//    Console.WriteLine("Nie działa!");
+//}
+
+
+//StaticClass.Test();
+//Console.WriteLine();
+
+
+//var date = DateTime.UtcNow; // Czas podany w utc
+//var polandTime = date.PolandTime(); // Wywołanie Extension Metody
+//var polandTime2 = date.PolandTime(5); // Wywołanie Extension Metody
+//Console.WriteLine(polandTime);
+//Console.WriteLine(polandTime2);
+
+//Console.WriteLine(polandTime.ToString("MM/dd.yyy HH:mm:ss:ff"));
+
+
+
 using Refs;
 
-Console.WriteLine("Hello, World!");
-
-int x = 5;
-Console.WriteLine(x);
-Console.WriteLine("Wysyłamy kopię do funkcji");
-var newValue = Incremnt(x);
-Console.WriteLine(newValue);
-
-int Incremnt(int x)
-{
-    return x += 5;
-}
-
-
-string text = "5fd";
-
-bool isGood = int.TryParse(text, out int parseValue);
-if (isGood == true)
-{
-    Console.WriteLine("Działa!");
-}
-else
-{
-    Console.WriteLine("Nie działa!");
-}
-
-
-StaticClass.Test();
-Console.WriteLine();
-
-
-var date = DateTime.UtcNow; // Czas podany w utc
-var polandTime = date.PolandTime(); // Wywołanie Extension Metody
-var polandTime2 = date.PolandTime(5); // Wywołanie Extension Metody
-Console.WriteLine(polandTime);
-Console.WriteLine(polandTime2);
-
-Console.WriteLine(polandTime.ToString("MM/dd.yyy HH:mm:ss:ff"));
+Date.ShowDate();
