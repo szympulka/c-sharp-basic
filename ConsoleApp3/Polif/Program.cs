95b797d03193f0563a7caf2cb9b817efbf947467
Szymon Pulka <szymon@hued.me>
2022-11-16 11:04:36 +0100

    animal voice

--- ConsoleApp3/Polif/Program.cs
+++ ConsoleApp3/Polif/Program.cs
@@ -3,8 +3,17 @@ using Polif;
 
 Console.WriteLine("Hello, World!");
 
+var animal = new Animal();
 var dog = new Dog();
 var cat = new Cat();
 
-Console.WriteLine(dog.Name);
-Console.WriteLine(cat.Name);
+Console.WriteLine($"{dog.Name} {dog.GetVoice()}");
+
+
+cat.Name = "Filemon";
+dog.Name = "Reksio";
+animal.Name = "Jestem Animal";
+
+Console.WriteLine($"{cat.Name} {cat.GetVoice()}");
+Console.WriteLine($"{dog.Name} {dog.GetVoice()}");
+Console.WriteLine($"{animal.Name} {animal.GetVoice()}");
